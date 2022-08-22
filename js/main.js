db3f59256228972571861c70be197bdf0709631d
Marsikwd <108618678+Marsikwd@users.noreply.github.com>
2022-08-22 12:02:08 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+function toggleMenu() {
+	const menuToggle = document.querySelector('.toggle');
+	const sidebar = document.querySelector('.sidebar');
+	menuToggle.classList.toggle('active');
+	sidebar.classList.toggle('active');
+}
+
+let linkWeb = document.querySelector('.web');
+
+linkWeb.addEventListener('click', function(e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/Web/');
+})
+
+let linkGame = document.querySelector('.game');
+
+linkGame.addEventListener('click', function(e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/Game-City/');
+})
+
+let linkClovery = document.querySelector('.clovery');
+
+linkClovery.addEventListener('click', function(e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/Clovery/');
+})
+
+let linkRest = document.querySelector('.rest');
+
+linkREst.addEventListener('click', function(e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/Clovery/');
+})
