972e41762545f2a2fe4a7c794cde29cbfd717aaa
Marsikwd <108618678+Marsikwd@users.noreply.github.com>
2022-08-31 17:06:13 +0300

    first-commit

--- js/main.js
+++ js/main.js
@@ -7,28 +7,35 @@ function toggleMenu() {
 
 let linkWeb = document.querySelector('.web');
 
-linkWeb.addEventListener('click', function(e) {
+linkWeb.addEventListener('click', function (e) {
 	e.preventDefault();
 	window.open('https://marsikwd.github.io/Web/');
 })
 
 let linkGame = document.querySelector('.game');
 
-linkGame.addEventListener('click', function(e) {
+linkGame.addEventListener('click', function (e) {
 	e.preventDefault();
 	window.open('https://marsikwd.github.io/Game-City/');
 })
 
 let linkClovery = document.querySelector('.clovery');
 
-linkClovery.addEventListener('click', function(e) {
+linkClovery.addEventListener('click', function (e) {
 	e.preventDefault();
 	window.open('https://marsikwd.github.io/Clovery/');
 })
 
 let linkRest = document.querySelector('.rest');
 
-linkREst.addEventListener('click', function(e) {
+linkRest.addEventListener('click', function (e) {
 	e.preventDefault();
-	window.open('https://marsikwd.github.io/Clovery/');
+	window.open('https://marsikwd.github.io/Restoran/');
+})
+
+let linkMarcy = document.querySelector('.Marcy');
+
+linkMarcy.addEventListener('click', function (e) {
+	e.preventDefault();
+	window.open('https://marsikwd.github.io/Mercy/');
 })
